c28abd000ca796f8177d4a88ba126814023a3cce
Vitaliy Stepanov <vstepanov@sfxdx.com>
2022-10-02 14:41:05 +0300

    fix images

--- src/App.tsx
+++ src/App.tsx
@@ -20,14 +20,25 @@ scoreboardImage.src = SpriteScoreboard
 function App() {
   const ref = useRef<HTMLCanvasElement>(null)
   const ref2 = useRef<HTMLCanvasElement>(null)
+  const [imageIsLoaded, setImageIsLoaded] = useState(0)
   const [selectedDifficult, setSelectedDifficult] = useState(Difficult.VeryEasy)
 
   useEffect(() => {
+    [borderImage, fieldImage, numbersImage, scoreboardImage].forEach((image) => {
+      if (image.complete) setImageIsLoaded(prevState => prevState + 1)
+      else {
+        image.onload = () => setImageIsLoaded(prevState => prevState + 1)
+      }
+    })
+  }, [])
+
+  useEffect(() => {
     if (!ref2.current || !ref.current) return
+    if (imageIsLoaded !== 4) return
     const game = new Game(ref.current, ref2.current, selectedDifficult)
     
     return () => game.removeListeners()
-  }, [ref2, ref, selectedDifficult])
+  }, [ref2, ref, imageIsLoaded, selectedDifficult])
 
   return (
     <div className="App">
